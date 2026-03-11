db072c2939deab3803b14f639d8f32f8ba0d06d9
TechnoVotes <107192485+TechnoVotes@users.noreply.github.com>
2026-03-11 23:02:17 +0530

    Add solution for entropy-node

---
+++ entropy-node/entropy-node.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def entropy_node(y):
+    """
+    Compute entropy for a single node using stable logarithms.
+    """
+    _,counts = np.unique(y, return_counts=True)
+    p=counts/counts.sum()
+    entropy = -np.sum(p * np.log2(p))
+    return float(entropy)
